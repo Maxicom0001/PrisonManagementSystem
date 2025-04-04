745a65a33526e683f783573164b4baac90cf7e21
Maxicom0001 <karate.lynx@gmail.com>
2025-04-04 22:43:35 +0200

    main file example

--- apps/server/src/main/java/server/App.java
+++
@@ -1,8 +0,0 @@
-package server;
-import static spark.Spark.*;
-
-public class App {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello, Spark!");
-    }
-}
---
+++ apps/server/src/main/java/server/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+
+        get("/hello", (req, res)->"Hello, world");
+
+        get("/hello/:name", (req,res)->{
+            return "Hello, "+ req.params(":name");
+        });
+    }
+}
