25c436cafc26aa55efba0457777ebf027bfc52fa
Nasakrator <jakub.batko06@gmail.com>
2025-04-07 13:27:08 +0200

    Main Layout & theme provider

---
+++ apps/web/components/layout/main-layout.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import type * as React from "react"
+import AppSidebar from "@/components/own/sidebar"
+import { HeaderProvider } from "../providers/header-title-provider"
+import Topbar from "../own/topbar"
+
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+interface MainLayoutProps {
+  children: React.ReactNode
+}
+
+export function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <HeaderProvider>
+      <SidebarProvider>
+        <div className="flex min-h-screen min-w-screen">
+          <AppSidebar />
+          <SidebarInset>
+            <Topbar/>
+            <main className="flex-1 p-4">{children}</main>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </HeaderProvider>
+  )
+}
+
+
