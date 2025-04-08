406645586786b19c64b0f95f7d42dafba4c5a4b8
Nasakrator <jakub.batko06@gmail.com>
2025-04-08 14:36:38 +0200

    Topbar animation

--- web/components/own/topbar.tsx
+++ web/components/own/topbar.tsx
@@ -4,6 +4,8 @@ import { useHeader } from "../providers/header-title-provider"
 import {
     SidebarTrigger
   } from "@/components/ui/sidebar"
+  import { motion } from "framer-motion"
+
 
 export default function Topbar() {
   const { title } = useHeader()
@@ -11,7 +13,7 @@ export default function Topbar() {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
       <SidebarTrigger className="-ml-1" />
-      <h1 className="text-xl font-semibold"><a href="/">Prison Management System</a> <span  style={{ color: `var(--foreground)` }} className="opacity-60">{title != "" ? "> " + title : ""}</span></h1>
+      <motion.h1 initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0}} transition={{duration: 0.3}} className="text-xl font-semibold"><a href="/">Prison Management System</a> <span  style={{ color: `var(--foreground)` }} className="opacity-60">{title != "" ? "> " + title : ""}</span></motion.h1>
     </header>
   )
 }
