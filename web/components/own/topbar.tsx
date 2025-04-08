5c85963c295c9fcf95574ff4f4e7b8433fe63bdd
Nasakrator <jakub.batko06@gmail.com>
2025-04-08 14:55:44 +0200

    topbar Animation changes

--- web/components/own/topbar.tsx
+++ web/components/own/topbar.tsx
@@ -10,10 +10,12 @@ import {
 export default function Topbar() {
   const { title } = useHeader()
 
+
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
       <SidebarTrigger className="-ml-1" />
-      <motion.h1 initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0}} transition={{duration: 0.3}} className="text-xl font-semibold"><a href="/">Prison Management System</a> <span  style={{ color: `var(--foreground)` }} className="opacity-60">{title != "" ? "> " + title : ""}</span></motion.h1>
+        <motion.a initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0}} transition={{duration: 0.3, type: "spring", stiffness: 200, damping: 12}} href="/" className="text-xl">Prison Management System</motion.a> 
+        <motion.span initial={{opacity: 0, x: -20}} animate={{opacity: 0.6, x: 0}} transition={{duration: 0.3, delay: 0.25, type: "spring", stiffness: 200, damping: 12}} className="opacity-60 text-xl ">{title != "" ? " > " + title : ""}</motion.span>
     </header>
   )
 }
