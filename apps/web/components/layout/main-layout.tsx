a4a75703e7b5f2c1d75d3b6bdbb2bbc8b6ee9fad
Nasakrator <jakub.batko06@gmail.com>
2025-04-08 11:22:47 +0200

    Adding provider for tanstack query in main layout

--- apps/web/components/layout/main-layout.tsx
+++ apps/web/components/layout/main-layout.tsx
@@ -10,21 +10,42 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
 interface MainLayoutProps {
   children: React.ReactNode
 }
+
+const queryClient = new QueryClient()
 
 export function MainLayout({ children }: MainLayoutProps) {
   return (
     <HeaderProvider>
       <SidebarProvider>
-        <div className="flex min-h-screen min-w-screen">
-          <AppSidebar />
-          <SidebarInset>
-            <Topbar/>
-            <main className="flex items-center justify-center h-full w-full">{children}</main>
-          </SidebarInset>
+        <QueryClientProvider client={queryClient}>
+          <div className="flex min-h-screen">
+          {/* Sidebar */}
+          <div className="w-64 shrink-0">
+            <AppSidebar />
+          </div>
+
+          {/* Main content area */}
+          <div className="flex-1 flex flex-col">
+            <SidebarInset className="flex flex-col flex-1 w-[calc(100vw-256px)]">
+              <Topbar />
+              <main className="flex-1 flex items-center justify-center w-[calc(100vw-256px)]">
+                {children}
+              </main>
+            </SidebarInset>
+          </div>
         </div>
+        </QueryClientProvider>
       </SidebarProvider>
     </HeaderProvider>
   )
