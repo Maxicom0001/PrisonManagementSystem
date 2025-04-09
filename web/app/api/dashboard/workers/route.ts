72929686b3ac20ceb70cef1b832b71c3cae3c1d1
GunterJP <barteksz110@gmail.com>
2025-04-09 12:43:50 +0200

    Api update

---
+++ web/app/api/dashboard/workers/route.ts
@@ -0,0 +1,43 @@
+import mysql from 'mysql2/promise';
+
+
+
+export async function GET() {
+  const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST || '127.0.0.1',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || 'jail',
+  });
+
+  try {
+    const [total] = await pool.query('SELECT COUNT(*) AS `total` FROM `workers`');
+    const [jobs] = await pool.query('SELECT COUNT(*) AS `jobs` FROM `jobs` WHERE aktywne = true;');
+
+    const response = {
+      total: total,
+      jobs: jobs  
+    };
+
+
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (err) {
+    console.error('Error executing query:', err);
+    return new Response(
+      JSON.stringify({ error: 'Internal server error' }),
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  } finally {
+    await pool.end();
+  }
+}
