4ee95a8d51fbe15c9280dfef2f0275d6e0fc5533
GunterJP <barteksz110@gmail.com>
2025-04-10 12:54:38 +0200

    Added dictionaries routes
    routes for cell_types, jobs, constraints and statuses

---
+++ web/app/api/dictionaries/cell_types/route.ts
@@ -0,0 +1,35 @@
+import mysql from "mysql2/promise";
+
+export async function GET() {
+    const pool = mysql.createPool({
+        host: process.env.MYSQL_HOST || "127.0.0.1",
+        user: process.env.MYSQL_USER || "root",
+        password: process.env.MYSQL_PASSWORD || "",
+        database: process.env.MYSQL_DATABASE || "jail",
+    });
+
+    try {
+        const [cellType] = await pool.query("SELECT * FROM cell_types;");
+
+        const response = {
+            cellType: cellType
+        };
+
+        return new Response(JSON.stringify(response), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (err) {
+        console.error("Error executing query:", err);
+        return new Response(JSON.stringify({ error: "Internal server error" }), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } finally {
+        await pool.end();
+    }
+}
