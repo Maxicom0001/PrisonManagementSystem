c363eb47630bedddab87ee6361e32615bea0e492
Maxicom0001 <karate.lynx@gmail.com>
2025-04-15 12:14:14 +0200

    api: added delete for edifices

--- web/app/api/edificies/[id]/route.ts
+++ web/app/api/edificies/[id]/route.ts
@@ -31,3 +31,44 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
         await pool.end();
     }
 }
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const pool = mysql.createPool({
+        host: process.env.MYSQL_HOST || "127.0.0.1",
+        user: process.env.MYSQL_USER || "root",
+        password: process.env.MYSQL_PASSWORD || "",
+        database: process.env.MYSQL_DATABASE || "jail",
+    });
+
+    const idToDelete = params.id;
+    try {
+        const [workers] = await pool.query("SELECT * FROM workers WHERE id_budynku = ?", [idToDelete]);
+
+        if ((workers as []).length > 0) {
+            return new Response(JSON.stringify({ error: "Cannot delete building: workers are assigned to it." }), {
+                status: 400,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+
+        const [result] = await pool.query("DELETE FROM edifices WHERE id = ?", [idToDelete]);
+
+        return new Response(JSON.stringify(result), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (err) {
+        console.error("Error executing query:", err);
+        return new Response(JSON.stringify({ error: "Internal server error" }), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } finally {
+        await pool.end();
+    }
+}
