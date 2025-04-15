de4db8c5783a0d932fcc8879392b6f6512ba0443
Maxicom0001 <karate.lynx@gmail.com>
2025-04-15 12:12:41 +0200

    api: added delete for cells

--- web/app/api/cells/[id]/route.ts
+++ web/app/api/cells/[id]/route.ts
@@ -34,3 +34,44 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
         await pool.end();
     }
 }
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const pool = mysql.createPool({
+        host: process.env.MYSQL_HOST || "127.0.0.1",
+        user: process.env.MYSQL_USER || "root",
+        password: process.env.MYSQL_PASSWORD || "",
+        database: process.env.MYSQL_DATABASE || "jail",
+    });
+
+    const idToDelete = params.id;
+    try {
+        const [convicts] = await pool.query("SELECT * FROM convicts WHERE id_celi = ?", [idToDelete]);
+
+        if ((convicts as []).length > 0) {
+            return new Response(JSON.stringify({ error: "Cannot delete cell: Convicts are assigned to this cell." }), {
+                status: 400,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+
+        const [result] = await pool.query("DELETE FROM cells WHERE id = ?", [idToDelete]);
+
+        return new Response(JSON.stringify(result), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (err) {
+        console.error("Error executing query:", err);
+        return new Response(JSON.stringify({ error: "Internal server error" }), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } finally {
+        await pool.end();
+    }
+}
