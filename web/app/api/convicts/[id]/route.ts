88f401a01b60060839b4f8e653a2a9d0f7ab5ddf
Maxicom0001 <karate.lynx@gmail.com>
2025-04-15 12:12:35 +0200

    api: added delete for convicts

--- web/app/api/convicts/[id]/route.ts
+++ web/app/api/convicts/[id]/route.ts
@@ -31,3 +31,33 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
         await pool.end();
     }
 }
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const pool = mysql.createPool({
+        host: process.env.MYSQL_HOST || "127.0.0.1",
+        user: process.env.MYSQL_USER || "root",
+        password: process.env.MYSQL_PASSWORD || "",
+        database: process.env.MYSQL_DATABASE || "jail",
+    });
+
+    const idToDelete = params.id;
+    try {
+        const [convict] = await pool.query("DELETE FROM convicts WHERE id = ?", [idToDelete]);
+
+        return new Response(JSON.stringify(convict), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (err) {
+        console.error("Error executing query:", err);
+        return new Response(JSON.stringify({ error: "Internal server error" }), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } finally {
+        await pool.end();
+    }
+}
