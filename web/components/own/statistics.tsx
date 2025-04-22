7ace52c99da2cf0d6fc49735693891d6c76aec9c
Nasakrator <jakub.batko06@gmail.com>
2025-04-22 17:57:43 +0200

    Statistics component integration with API

--- web/components/own/statistics.tsx
+++ web/components/own/statistics.tsx
@@ -3,22 +3,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { ScrollingLoader } from "@/components/own/scrolling-loader";
 import { motion } from "framer-motion";
+import fetchData from "@/components/api/fetch-data";
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const fetchData = async () => {
-    await delay(2000); // Simulate network delay
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    if (!response.ok) {
-        throw new Error("Network response was not ok");
-    }
-    return await response.json();
-};
 
 export function PrisonersCounter() {
     const { data, isLoading, isError, error } = useQuery({
-        queryKey: ["data"],
-        queryFn: fetchData,
+        queryKey: ["dashboard/prisoners"],
+        queryFn: () => fetchData("api/dashboard/prisoners"),
         refetchOnWindowFocus: false,
         retry: false,
     });
@@ -28,15 +19,15 @@ export function PrisonersCounter() {
 
     return (
         <motion.p className="text-sm text-muted-foreground" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.3 }}>
-            Prisoners: {data.userId}
+            Prisoners: {data.prisoners.total}
         </motion.p>
     );
 }
 
 export function WorkersCounter() {
     const { data, isLoading, isError, error } = useQuery({
-        queryKey: ["data"],
-        queryFn: fetchData,
+        queryKey: ["dashboard/workers"],
+        queryFn: () => fetchData("api/dashboard/workers"),
         refetchOnWindowFocus: false,
         retry: false,
     });
@@ -46,15 +37,15 @@ export function WorkersCounter() {
 
     return (
         <motion.p className="text-sm text-muted-foreground" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.3 }}>
-            Workers: {data.userId}
+            Workers: {data.workers.total}
         </motion.p>
     );
 }
 
 export function ActiveJobsCounter() {
     const { data, isLoading, isError, error } = useQuery({
-        queryKey: ["data"],
-        queryFn: fetchData,
+        queryKey: ["dashboard/workers"],
+        queryFn: () => fetchData("api/dashboard/workers"),
         refetchOnWindowFocus: false,
         retry: false,
     });
@@ -64,25 +55,8 @@ export function ActiveJobsCounter() {
 
     return (
         <motion.p className="text-sm text-muted-foreground" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.3 }}>
-            Active Jobs: {data.userId}
+            Active Jobs: {data.workers.jobs}
         </motion.p>
     );
 }
 
-export function LastUpdateCounter() {
-    const { data, isLoading, isError, error } = useQuery({
-        queryKey: ["data"],
-        queryFn: fetchData,
-        refetchOnWindowFocus: false,
-        retry: false,
-    });
-
-    if (isLoading) return <ScrollingLoader indicatorColor="bg-gray-200"></ScrollingLoader>;
-    if (isError) return <div>Error: {error.message}</div>;
-
-    return (
-        <motion.p className="text-sm text-muted-foreground" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.3 }}>
-            Last update: {data.userId}
-        </motion.p>
-    );
-}
