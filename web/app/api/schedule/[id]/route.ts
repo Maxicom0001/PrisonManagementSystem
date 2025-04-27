1c47b5053620453849a64ff76c170bad4a95781b
Nasakrator <jakub.batko06@gmail.com>
2025-04-27 16:46:03 +0200

    schedule [id] API GET & PATCH add

--- web/app/api/schedule/[id]/route.ts
+++ web/app/api/schedule/[id]/route.ts
@@ -1,4 +1,5 @@
 import connectDB from "@/components/api/connectDB";
+import { NextRequest } from "next/server";
 
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
     const pool = connectDB();
@@ -30,10 +31,10 @@ export async function DELETE(request: Request, { params }: { params: { id: strin
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     const pool = connectDB();
 
-    const id = params.id; // Assuming you have the id from the request parameters
+    const id = params.id;
 
     try {
-        const [rows] = await pool.query(`SELECT * FROM \`schedule\` WHERE id = ${id}`);
+        const [rows] = await pool.query(`SELECT * FROM \`schedule\` WHERE id = ?`, [id]);
 
         return new Response(JSON.stringify(rows), {
             status: 200,
@@ -53,3 +54,43 @@ export async function GET(request: Request, { params }: { params: { id: string }
         await pool.end();
     }
 }
+
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+    const pool = connectDB();
+
+    const id = params.id; // Assuming you have the id from the request parameters
+
+    try {
+
+        const body = await req.json();
+        console.log("Request body:", body); // Log the request body for debugging
+
+        const time = body.time;
+        const title = body.title;
+
+        const query = `UPDATE \`schedule\` SET time = ?, title = ? WHERE id = ?`;
+
+        console.log("Query:", query);
+        console.log("Parameters:", [time, title, id]);
+
+        const [rows] = await pool.query(query, [time, title, id]);
+
+        return new Response(JSON.stringify(rows), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (err) {
+        console.error("Error executing query:", err);
+        return new Response(JSON.stringify({ error: "Internal server error" }), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } finally {
+        await pool.end();
+    }
+}
+
